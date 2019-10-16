40e3cfe707d75cb669428f08accbd1085d7ef4d9
Meowmerry <meow3525@gmail.com>
2019-10-16 14:58:09 +0700

    Second commited  : Responsive Resume Page
    SODecond commited Changes #modified:   css/stly.css :   index.html
    
    committed: 	modified:   JS/javaScrip.js :   css/stly.css :   index.html
    
    Committed modified:   index.html
    
    Committed for modified: JS/javaScrip.js :   css/stly.css :   index.html
    
    Changes to be committed:   JS/javaScrip.js
    
    Changes to be committed: modified:   JS/javaScrip.js :wmodified:   index.html

--- JS/javaScrip.js
+++ JS/javaScrip.js
@@ -1,59 +1,55 @@
 
     // Function to change webpage background color
-        const changeColor = document.getElementById('changeColor'),
-        colors      = ['red', 'green', 'blue','pink'];
-        let   colorIndex  = 0;
+    const changeColor = document.getElementById('changeColor'),
+    colors      = ['red', 'green', 'blue','pink'];
+    let   colorIndex  = 0;
 
-        changeColor.addEventListener('click', () => {
-        document.body.style.backgroundColor = colors[colorIndex];      
-        colorIndex = (colorIndex + 1) % colors.length
-        });
+    changeColor.addEventListener('click', () => {
+    document.body.style.backgroundColor = colors[colorIndex];      
+    colorIndex = (colorIndex + 1) % colors.length
+    });
 
-    // Function to change name
-    function mynameFunction() {
-        var name;
-        var username = prompt("Please enter your name:", "Thasanee Puttamadilok");
-        if (username == null || username == "") {
-          name = "User cancelled the prompt.";
-        } else {
-          name = "Hello " + username + "! How are you today?";
-        }
-        document.getElementById("changename").innerHTML = name;
-      }
-    // Function to add and remove skills
-    
-     
-
-
-    // Function to add and remove skills
-    var listitem = 0;
-    function addToList() {
-        if (listitem >=4) {
-            listitem = 0;
-        }
-        if (listitem == 0) {
-        var skills = "Good skill in English and know Spanish.";
-        }
-        if (listitem == 1) {
-        var skills = "Strategic Sales & Marketing Campaigns.";
-        }
-        if (listitem == 2) {
-        var skills = "Microsoft Word, Excels, Adobe Photoshop, Power Point, E-mails, Internet.";
-        }
-        if (listitem == 3) {
-        var skills = "Strong Sales Presentations / Closing success";
-        }
-        listitem++
-
-        var newnode=document.createElement("LI");
-        var textnode = document.createTextNode(skills);
-        newnode.append(textnode);
-        document.getElementById("myskills").appendChild(newnode);
-
+// Function to change name
+function mynameFunction() {
+    var name;
+    var username = prompt("Please enter your name:", "Thasanee Puttamadilok");
+    if (username == null || username == "") {
+      name = "User cancelled the prompt.";
+    } else {
+      name = "Hello " + username + "! How are you today?";
     }
+    document.getElementById("changename").innerHTML = name;
+  }
 
-   function removeFromList(){
-        var list = document.getElementById("myskills");
-        list.removeChild(list.childNodes[0]);
+// Function to add and remove skills
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+      alert("You must write something!");
+  } else {
+      document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+
+  span.appendChild(txt);
+  li.appendChild(span);
+  span.className = "close";   
+  for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+  var div = this.parentElement;
+  div.style.display = "none";
+}
+}
+}
+  function removeFromList(){
+      var list = document.getElementById("myUL");
+       list.removeChild(list.childNodes[0]);
    }
+
 
